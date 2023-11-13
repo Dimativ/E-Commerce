b351fa57db8772ca34775ed9b29badb1ffda10f7
Dimativ <Dimativtoptinker@gmail.com>
2023-11-13 20:00:24 +0200

    feat: added settings page with possibility to rename and delete the store

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -13,12 +13,18 @@ export function MainNav({
     const params = useParams();
 
 
-    const routes = [{
+    const routes = [
+        {
+            href: `/${params.storeId}`,
+            label: 'Overview',
+            active: pathname === `/${params.storeId}`,
+        },
+        {
         href: `/${params.storeId}/settings`,
         label: 'Settings',
         active: pathname === `/${params.storeId}/settings`,
-
-    },];
+        },
+    ];
 
     return (
         <nav className={cn("flex items-center space-x-4 lg:space-x-6", className)}>
