cc5858197415b8e5d9e92ffb19c9faabcfa48262
Dimativ <Dimativtoptinker@gmail.com>
2023-11-17 20:38:23 +0200

    feat: added sizes page with pagination, filtering feat: added component api-list for documentation feat: added routes for sizes fix: fixed some imports for better experience

--- app/(dashboard)/[storeId]/(routes)/sizes/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/sizes/page.tsx
@@ -1,12 +1,12 @@
 import {format} from "date-fns";
 
-import {BillboardClient} from "./components/client";
+import {SizeClient} from "./components/client";
 import prismadb from "@/lib/primsmadb";
-import {BillboardColumn} from "./components/columns";
+import {SizeColumn} from "./components/columns";
 
-const BillboardsPage = async ({params}: {params: {storeId: string}}) => {
+const SizesPage = async ({params}: {params: {storeId: string}}) => {
 
-    const billboards = await prismadb.billboard.findMany({
+    const sizes = await prismadb.size.findMany({
         where: {
             storeId: params.storeId,
         },
@@ -15,19 +15,20 @@ const BillboardsPage = async ({params}: {params: {storeId: string}}) => {
         }
     });
 
-    const formattedBillboards: BillboardColumn[] = billboards.map((item) => ({
+    const formattedSizes: SizeColumn[] = sizes.map((item) => ({
         id: item.id,
-        label: item.label,
+        name: item.name,
+        value: item.value,
         createdAt: format(item.createdAt, "MMMM do, yyyy")
     }));
 
     return (
         <div className="flex-col">
             <div className="flex-1 space-y-4 p-8 pt-6">
-                <BillboardClient data={formattedBillboards}/>
+                <SizeClient data={formattedSizes}/>
             </div>
         </div>
     );
 }
 
-export default BillboardsPage;
+export default SizesPage;
